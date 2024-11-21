19492c289c8e382ee99bb80f483718c94cf98406
nidhi549 <Nidhi.Mistry.4x22@outlook.com>
2024-11-21 12:30:41 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'bindings/expense_binding.dart';
-import 'views/home_screen.dart';
+import 'package:path_provider/path_provider.dart';
 
+import 'bindings/expense_binding.dart';
+import 'models/expense_model.dart'; // Import your Expense model
+import 'views/home_screen.dart';
+a
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  await Hive.openBox('expenses'); // Open the Hive box to store expenses
+  final appDocumentDirectory = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter(); // Initialize Hive
+
+  // Register the Expense model adapter
+  Hive.registerAdapter(ExpenseAdapter());
+
+  // Open the Hive box for expenses
+  await Hive.openBox<Expense>('expenses');
+
   runApp(MyApp());
 }
 
